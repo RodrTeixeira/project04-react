5367d2aee09818ddec86cf612ce11e9edca558e9
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-04-29 16:28:53 -0300

    :zap: home structure

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { globalStyles } from '../styles/global'
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from '../styles/pages/app'
 
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
