ed4eb254a5cd714724d1fa1b9c7bd3e3c31de608
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-05-01 08:50:12 -0300

    :zap: data fetching

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -7,18 +7,32 @@ import Image from "next/image";
 import camiseta1 from "../assets/camisetas/1.png"
 import camiseta2 from "../assets/camisetas/2.png"
 import camiseta3 from "../assets/camisetas/3.png"
+import camiseta4 from "../assets/camisetas/4.png"
 
 import 'keen-slider/keen-slider.min.css' 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [list, setList] = useState<number[]>([])
+
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
       spacing: 48,
     },
   })
+
+  useEffect(() => {
+    setTimeout(() => {
+      setList([1, 2, 3])
+    }, 2000)
+  },[])
+
   return (
    <HomeContainer ref={sliderRef} className='keen-slider'>
+
+      <pre>{JSON.stringify(list)}</pre>
 
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
@@ -45,7 +59,7 @@ export default function Home() {
       </Product>
 
       <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
+        <Image src={camiseta4} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
